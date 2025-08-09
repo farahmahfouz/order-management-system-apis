a4e0b3107b6e6f1c2740994af8d9b7b2ab0e4123
farahmahfouz <farahmahfouz11@gmail.com>
2025-08-09 15:22:10 +0300

    handle cookie option

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,6 +10,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 50 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'None',
   };
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
